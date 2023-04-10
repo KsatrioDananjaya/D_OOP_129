23b7ff79e68ba641d0c63c273cdc53f01b1573d0
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-04-10 15:20:33 +0700

    menambahkan procedule

--- oop2/oop2.cpp
+++ oop2/oop2.cpp
@@ -16,6 +16,21 @@ private:
     int sks;
     string kode;
     string namamk;
+public:
+    void inputmk() {
+        cout << "Masukkan SKS: ";
+        cin >> sks;
+        cout << "Masukkan Kode: ";
+        cin >> kode;
+        cout << "Masukkan Nama Mata kuliah: ";
+        cin >> namamk;
+    }
+    void tampilmk() {
+        cout << "\nSKS Mata Kuliah = " << sks;
+        cout << "\nKode Mata Kuliah = " << kode;
+        cout << "\nNama Mata Kuliah = " << namamk << endl;
+    }
+};
 
 int main()
 {
